738da949dc14d1625140460f77173fd678cbb394
Han Chee Tan <hancheester@gmail.com>
2019-11-16 23:45:58 +0000

    Add comments to show bounds of source files

--- preprocessor/multiply.h
+++ preprocessor/multiply.h
@@ -1,1 +1,5 @@
+// start of multiply.h
+
 int multiply(int, int);
+
+// end of multiply.h
--- preprocessor/program.c
+++ preprocessor/program.c
@@ -1,6 +1,10 @@
+// start of program.c
+
 #include "multiply.h"
 
 int main()
 {
 	multiply(4, 5);
 }
+
+// end of program.c
