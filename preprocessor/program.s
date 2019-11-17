083c65b9c706f2427eee5adbf6ba5657c434ad11
Han Chee Tan <hancheester@gmail.com>
2019-11-17 00:35:12 +0000

    After executing gcc -C -P -save-temps program.c

---
+++ preprocessor/program.s
@@ -0,0 +1,28 @@
+	.file	"program.i"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	movl	$5, %edx
+	movl	$4, %ecx
+	call	multiply
+	movl	$7, %edx
+	movl	$6, %ecx
+	call	multiply
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	multiply;	.scl	2;	.type	32;	.endef
