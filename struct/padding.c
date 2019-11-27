69dfd3d242c0b7930c8dacb8a97a44900edc9d56
Han Chee Tan <hancheester@gmail.com>
2019-11-27 23:10:29 +0000

    Due to padding, sizeof(Layout) is 12 bytes

---
+++ struct/padding.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+typedef struct 
+{
+	short first; // xx__ -> 2 bytes
+	int second;  // xxxx -> 4 bytes
+	short third; // xx__ -> 2 bytes
+	// However, due to structure padding, xx__ is 4 bytes (xx 2 bytes + __ 2 bytes) where _ is empty / padding
+	// Thus, sizeof(Layout) will be 4 + 4 + 4 = 12 bytes
+} Layout;
+
+int main()
+{
+	printf("short %d\n", (int) sizeof(short));
+	printf("int %d\n", (int) sizeof(int));
+	printf("struct Layout %d\n", (int) sizeof(Layout));	
+}
