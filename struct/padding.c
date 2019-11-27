d4d57767e008696274e8722ddb2c2867159840c1
Han Chee Tan <hancheester@gmail.com>
2019-11-27 23:13:20 +0000

    By re-arranging struct members, sizeof(Layout) is 8 bytes now

--- struct/padding.c
+++ struct/padding.c
@@ -2,11 +2,16 @@
 
 typedef struct 
 {
-	short first; // xx__ -> 2 bytes
-	int second;  // xxxx -> 4 bytes
-	short third; // xx__ -> 2 bytes
+	//short first; // xx__ -> 2 bytes
+	//int second;  // xxxx -> 4 bytes
+	//short third; // xx__ -> 2 bytes
 	// However, due to structure padding, xx__ is 4 bytes (xx 2 bytes + __ 2 bytes) where _ is empty / padding
 	// Thus, sizeof(Layout) will be 4 + 4 + 4 = 12 bytes
+
+	short first; // xx -> 2 bytes
+	short third; // xx -> 2 bytes
+	int second;  // xxxx -> 4 bytes
+	// sizeof(Layout) will be 2 + 2 + 4 = 8 bytes
 } Layout;
 
 int main()
