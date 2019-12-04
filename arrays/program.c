3c15dadc5a5119e423f0320d25c480568475bc6f
Han Chee Tan <hancheester@gmail.com>
2019-12-04 21:59:49 +0000

    Using function and pointer to find the largest number in array

--- arrays/program.c
+++ arrays/program.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
+int * printLargest(int * begin, int * end)
+{
+	if (begin == end) return 0;
+
+	int * largest = begin;
+
+	for(; begin != end; begin++)
+	{
+		printf("%d\n", *begin);
+
+		if (*largest < *begin)
+		{
+			largest = begin;
+		}
+	}
+
+	return largest;
+}
+
 int main()
 {
 	int numbers[3] = {12, 13, 14};
@@ -18,5 +37,16 @@ int main()
 	for(; p != end; p++)
 	{
 		printf("%d\n", *p);
+	}
+
+	printf("\n");
+
+	int list[] = {2, 1, 4, 2, 15, 2, 4};
+	size = sizeof(list) / sizeof(list[0]);
+	int * largest = printLargest(list, list + size);
+
+	if (largest)
+	{
+		printf("largest %d\n", *largest);
 	}
 }
